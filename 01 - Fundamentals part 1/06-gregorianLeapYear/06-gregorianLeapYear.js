4d42691948a91ae7185071208a761b4bfa5f2cc4
Leonardo Ghiggino <lghiggino@gmail.com>
2020-07-17 07:55:56 -0300

    02/01/06 - gregorian leap year w/ form and submit

---
+++ 01 - Fundamentals part 1/06-gregorianLeapYear/06-gregorianLeapYear.js
@@ -0,0 +1,17 @@
+console.log("its a new day. Be kind to yourself and the people around you.");
+
+
+const myForm = document.querySelector("#myForm");
+myForm.addEventListener("submit", leapYearCalculator);
+
+const para = document.querySelector("#result")
+
+
+function leapYearCalculator(e){
+    e.preventDefault();
+    let valor = myForm.ano.value;
+    console.log(valor);
+    if(valor % 4 === 0 && valor % 100 === 0 && valor %400 === 0){
+        para.textContent = `${valor} is a leap year.`;
+    } else para.textContent=`${valor} is not a leap year.`
+};

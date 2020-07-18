7c2c6bff7a9fc8101b08b2cdf53107d8d8ca3f08
Leonardo Ghiggino <lghiggino@gmail.com>
2020-07-18 15:27:23 -0300

    02/01/07 - fully functional awith weekdays object

--- 01 - Fundamentals part 1/07 - JanuaryFirstSunday/07 - JanuaryFirstSunday.js
+++ 01 - Fundamentals part 1/07 - JanuaryFirstSunday/07 - JanuaryFirstSunday.js
@@ -1,3 +1,5 @@
+console.log("believe in yourself")
+
 const myForm = document.querySelector("#form");
 myForm.addEventListener("submit", janFirstSunday);
 
@@ -5,5 +7,21 @@ const para = document.querySelector("#result");
 
 function janFirstSunday(e){
     e.preventDefault();
-    //funçãopra valer a partir daqui
-}
+    //função pra valer a partir daqui
+    let userYear = myForm.yearInp.value
+    let myDate = new Date(userYear+",1,1");
+    let janFirstDay = myDate.getDay()
+    let week = {
+        0: "SUNDAY! Yes!",
+        1: "monday",
+        2: "tuesday",
+        3: "wednesday",
+        4: "thursday",
+        5: "friday",
+        6: "saturday"}
+    console.log(janFirstDay);
+    console.log(week[janFirstDay]);
+        para.textContent = `${userYear}'s January first is a ${week[janFirstDay]}`;
+    }
+
+

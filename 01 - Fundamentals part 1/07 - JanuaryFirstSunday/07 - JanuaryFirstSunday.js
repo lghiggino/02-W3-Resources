92755ff1e8dfce0da5506fa67cb49fa81c8d0e62
Leonardo Ghiggino <lghiggino@gmail.com>
2020-07-18 13:38:14 -0300

    02/01/07 - January First is Sunday? Files

---
+++ 01 - Fundamentals part 1/07 - JanuaryFirstSunday/07 - JanuaryFirstSunday.js
@@ -0,0 +1,9 @@
+const myForm = document.querySelector("#form");
+myForm.addEventListener("submit", janFirstSunday);
+
+const para = document.querySelector("#result");
+
+function janFirstSunday(e){
+    e.preventDefault();
+    //funçãopra valer a partir daqui
+}

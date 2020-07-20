86a643363186350db2a2c989000057dac2298eed
Leonardo Ghiggino <lghiggino@gmail.com>
2020-07-20 07:42:15 -0300

    02/01/08 - reset function and bugged out

--- 01 - Fundamentals part 1/08 - random1to10/08 - random1to10.js
+++ 01 - Fundamentals part 1/08 - random1to10/08 - random1to10.js
@@ -26,22 +26,27 @@ const content2 = document.getElementById("content2");
 const para2 = document.createElement("p");
 const para3 = document.createElement("p");
 content2.appendChild(para2);
-content2.appendChild(para3)
+content2.appendChild(para3);
 
 const form2 = document.getElementById("form2");
-form2.addEventListener("submit", compare2)
+form2.addEventListener("submit", compare2);
+const button2 = document.getElementById("playerBtn2");
 
-const playerNumber2 = document.getElementById("playerNumber2")
-let roundCount = 0
+const playerNumber2 = document.getElementById("playerNumber2");
+let roundCount = 0;
 let computerChoice2 = Math.floor(Math.random()*10+1);
 
 function compare2(e){
     e.preventDefault();
     let playerChoice2 = form2.playerNumber2.value;
-    console.log(computerChoice2);
-    console.log(playerChoice2);
+
+    function reset(){
+        roundCount = 0;
+        computerChoice2 = Math.floor(Math.random()*10+1);
+    }
     if (computerChoice2 == playerChoice2){
         para2.textContent = `Congratulations! You win! Both you and the computer chose ${playerChoice2}. Click the button to restart`
+        reset();
     } else if (computerChoice2 > playerChoice2){
         roundCount++
         para2.textContent = `The number you chose is smaller than the computer choice. Round ${roundCount}.`
@@ -49,10 +54,11 @@ function compare2(e){
         roundCount++
         para2.textContent = `The number you chose is bigger than the computer choice. Round ${roundCount}. `
     }
+    
+    //reseting the game
     if(roundCount === 3){
-        para3.textContent = "Game Over. Click the button to restart.";
-        computerChoice2 = Math.floor(Math.random()*10+1);
-        roundCount = 0;
+        para3.textContent = `The computer chose ${computerChoice2}. Game Over. Click the button to restart.`;
+        reset();
     } else para3.textContent = ""
 
 
